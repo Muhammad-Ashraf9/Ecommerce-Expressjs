95300986a93d1bf665bfd7a88678862400d01474
Muhammad Ashraf <mohash805@gmail.com>
2023-04-30 01:42:44 +0300

    added templating engine

--- app.js
+++ app.js
@@ -8,14 +8,18 @@ const shopRoutes = require("./routes/shop");
 
 const app = express();
 
+app.set("view engine", "ejs");
+// app.set("view", "views"); // default
+
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminRoutes);
+app.use("/admin", adminRoutes.router);
 app.use(shopRoutes);
 
 app.use((req, res) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  // res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  res.status(404).render("404", { pageTitle: "Page Not Found" });
 });
 
 app.listen(3000, () => {
--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,14 @@
 const express = require("express");
 
 const path = require("path");
+const adminData = require("./admin");
 
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
+  // res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
+  const products = adminData.products;
+  res.render("shop", { pageTitle: "Shop", products: products, path: "/" });
 });
 
 module.exports = router;
